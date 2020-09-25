0786676f6e2f5632fc2b8dda3480dd4eaed5797e
john-woods-adm <71862092+john-woods-adm@users.noreply.github.com>
2020-09-25 08:54:51 -0400

    changed it from cert auth to user

--- wvd_menu_script.ps1
+++ wvd_menu_script.ps1
@@ -1,40 +1,29 @@
-﻿
 
-function login-menu {
+#john.woods.
+
+############################### Login TO RDS ###################################
+function login-menu-rds {
     Clear-Host
     Write-Host "================ Login with Remote Desktop Admin Credentials to Continue ================"
-    Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
-    #Write-Host "You are currently $loggedIn"
-    #Write-Host "1: Press 'Y' to continue"
-    #Write-Host "Q: Press 'Q' to quit."
+    $rdsAccount = Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
+    return $rdsAccount
 }
-#try {
-#    Get-RdsContext -ErrorAction Stop
-#}
-#catch {
-#    Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
-#}
 
-#Do RDS auth
-#do
-# {
-#    login-menu
-#    
-#    $selection = Read-Host "Please make a selection"
-#    switch ($selection)
-#    {
-#    'Y' {
-#        Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
-#    }
-#    }
-#    pause
-# }
-# until ($selection -eq 'q')
-# if ($selection -eq 'q'){break}
 
-login-menu
+Clear-Host
+Write-Host "================ Login with Azure Admin Credentials to Continue ================"
+$azAccount = Connect-AzureRMAccount
+
+
+
+#show login screen
+#$azAccount = login-menu-azure
+$rdsAccount = login-menu-rds
+
+#only used for testing
+
 try {
-
+    #test credentials
     $rdsContext = Get-RdsContext -ErrorAction Stop
 }
 catch {
@@ -42,41 +31,83 @@ catch {
     do
     {
         $selection = Read-Host "Try Again? (Y/N)"
+        #login failed, run login again is select Y otherwise break
         if ($selection -ne 'n'){$rdsContext = Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"}
         
     }
-    until (($selection -eq 'n') -or ($rdsContext -ne $null))
+    until (($selection -eq 'n') -or ($rdsContext -ne $null))#run until say N or auth goes through
     if ($selection -eq 'n'){break}
     
-
+    #if authenticated prompt for and check account again tenant
 }
 
-function rds-info-menu {
-    Clear-Host
-    Write-Host "================ Collecting RDS Info ================"
-    Add-RdsAccount -DeploymentUrl "https://rdbroker.wvd.microsoft.com"
-    #Write-Host "You are currently $loggedIn"
-    #Write-Host "1: Press 'Y' to continue"
-    #Write-Host "Q: Press 'Q' to quit."
+######################### Enter Tenant and Host Pool Info ############################################
+
+function tenant-pool-menu {
+    Write-Host "================ Enter Tenant & Host Pool Information ================"
+    $rdsTenant = Read-Host "Please enter the RDS Tenant you wish to access: "
+    $rdsHostPool = Read-Host "Please enter the RDS Host Pool you wish to access: "
+    return $rdsTenant, $rdsHostPool
 }
-$rdsTenant = Read-Host "Please enter the RDS Tenant you wish to access: "
-$rdsHostPool = Read-Host "Please enter the RDS Host Pool you wish to access: "
-    
+
+
+$rdsTenant, $rdsHostPool = tenant-pool-menu
+Get-RdsUserSession -TenantName $rdsTenant -HostPoolName $rdsHostPool
+Clear-Host
 $rdsUser = $rdsContext.UserName
+try {#try tenant and pool info for given user
+    $userSession = Get-RdsUserSession -TenantName $rdsTenant -HostPoolName $rdsHostPool -ErrorAction Stop
+    #try command to test auth to tenant and pool
+}
+catch {
+    do
+    {
+    
+
+        Write-Host "$rdsUser doesn't have acces to the Tenant: $rdsTenant and/or Host Pool: $rdsHostPool"
+        Write-Host "1: Press '1' to login as a different RDS user."
+        Write-Host "2: Press '2' to try a different tenant and host pool."
+        Write-Host "Q: Press 'Q' to quit."
+        #user doesn't have access to given tenant or pool
+        #retry tenant & pool, or sign in as different user
+        $selection = Read-Host "Please make a selection"
+        switch ($selection)
+        {
+            '1' {
+                $rdsAccount = login-menu-rds
+                $rdsContext = Get-RdsContext -ErrorAction Stop
+                $rdsUser = $rdsContext.UserName
+            }
+            '2' {
+                $rdsTenant, $rdsHostPool = tenant-pool-menu
+            }
+        }
+        $userSession = Get-RdsUserSession -TenantName $rdsTenant -HostPoolName $rdsHostPool
+        $rdsContext
+        $rdsTenant
+        $rdsHostPool
+        $userSession
+        pause
+
+     }
+     until (($selection -eq 'n') -or ($userSession -ne $null))#run until say N or auth goes through
+     if ($selection -eq 'n'){break}
+    
+    
+ }
+
+################################# RDS Menu Options ########################
+
 function rds-menu {
     Clear-Host
     Write-Host "================ RDS Menu ================"
     Write-Host "Welcome $rdsUser, make a selection below for what you want to do: "
-    Write-Host "1: Press '1' to list the number of logged on users."
-    Write-Host "2: Press '2' to show where a specific user is logged in."
-    Write-Host "3: Press '3' to show all users logged into a specific machine."
-    Write-Host "4: Press '4' to add a user to the default app group."
-    Write-Host "5: Press '5' to add a web browser to a user profile."
-    Write-Host "6: Press '6' to remove a web browser from a user profile."
-    Write-Host "7: Press '7' to force a user logoff."
+    Write-Host "1: Press '1' to list all logged in users."
+    Write-Host "2: Press '2' to add a user to an app group."
+    Write-Host "3: Press '3' to list all app groups."
+    Write-Host "4: Press '4' to force a user logoff."
     Write-Host "Q: Press 'Q' to quit."
 }
-
 do
  {
     
@@ -87,27 +118,24 @@ do
     switch ($selection)
     {
     '1' {
-    'list logged in users'
+        'get logged in users'
+        Get-RdsUserSession -TenantName $rdsTenant -HostPoolName $rdsHostPool
     } 
     '2' {
-    'show where a user is logged in'
+        'add a user to an app group'
+        $appGroupName = Read-Host "App Group Name: "
+        $upn = Read-Host "UPN for user ex. bsmith@nrtbus.com: "
+        Add-RdsAppGroupUser -TenantName $rdsTenant -HostPoolName $rdsHostPool -AppGroupName $appGroupName -UserPrincipalName $upn
     } 
     '3' {
-      'show all users logged into cp'
+      'show all app groups'
+        Get-RdsAppGroup -TenantName $rdsTenant -HostPoolName $rdsHostPool
     }
     '4' {
-    'adding a user to the default app group'
-    } 
-    '5' {
-    'adding web browser to user profile'
-    } 
-    '6' {
-      'remove web browser from user profile'
+    'logoff a user'
+    $upn = Read-Host "UPN for user to logoff ex. bsmith@nrtbus.com: "
+    Get-RdsUserSession -TenantName $rdsTenant -HostPoolName $rdsHostPool | where { $_.UserPrincipalName -eq $upn } | Invoke-RdsUserSessionLogoff -NoUserPrompt
     }
-    '7' {
-      'force user logoff'
-    }
-
     }
     pause
  }
